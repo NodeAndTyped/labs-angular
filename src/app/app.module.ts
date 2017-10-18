fc7ba07d831991187bdc8f5387a4a7b72b7ba8be
romain.lenzotti <rom.lenzotti@gmail.com>
2017-10-18 10:24:49 +0200

    chore: update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import {MaterialModule} from '@angular/material';
-import { AppComponent } from './app.component';
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {MatButtonModule, MatCheckboxModule} from "@angular/material";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        MaterialModule,
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
